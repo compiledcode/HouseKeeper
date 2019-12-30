61a9484f0b010228b12fe125cb7673013c8ced02
devccs <ebw@startmail.com>
2019-12-30 12:19:18 -0800

    added first round rough draft python embedded into C++

---
+++ cmake-build-debug/CMakeFiles/embedpy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/embedpy.dir/main.cpp.o"
+  "embedpy"
+  "embedpy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/embedpy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
